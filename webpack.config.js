f46b63aae548e612d3772a546a4d70bd3de6043c
Bricks666 <tconckoff82@gmail.com>
2022-01-09 23:11:56 +0300

    some changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
 	mode: "production",
@@ -20,6 +21,7 @@ module.exports = {
 	resolve: {
 		extensions: [".ts", ".js"],
 	},
+	externals: [nodeExternals()],
 	module: {
 		rules: [
 			{
